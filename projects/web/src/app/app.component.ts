e8f6819a0025c7612e60b387d2862623148490ce
gustavo-morales <eduard.ramirez@insoftar.com>
2021-08-08 09:05:47 -0500

    animacion alert

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var UIkit: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web';
+
+  ngOnInit() {
+    UIkit.modal('#contact-info').show();
+  }
 }
